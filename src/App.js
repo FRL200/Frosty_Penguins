4cc678c8f56a25c6dbd995d8a04d87bbb43e0bb5
FRL200 <fernando.rauseo@ufl.edu>
2022-03-09 12:41:55 -0500

    Pushing fixes to make base of project work

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { BrowserRouter as Router,Route } from 'react-router-dom';
+//import 'semantic-ui-css/semantic.min.css';
+//fix this ^^
+
+
+import './App.css';
+
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <div><h1>Hello</h1></div>
+    //  <Router>
+    //    <Route exact path="/" component={Home} />
+    //  </Router>
+    //fix this too ^^
+  );
+}
+
+export default App;
