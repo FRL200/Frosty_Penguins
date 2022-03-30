4e127ba2610d2b6b74f0c06601de2b4c709f6290
Eitan Kogutek <eitan.kogutek@ufl.edu>
2022-03-30 17:54:07 -0400

    Button Addition

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { BrowserRouter as Router,Route } from 'react-router-dom';
+import { BrowserRouter as Router,Link,Route } from 'react-router-dom';
+import {ButtonComponent} from '@syncfusion/ej2-react-buttons';
 //import 'semantic-ui-css/semantic.min.css';
 //fix this ^^
 
@@ -10,12 +11,21 @@ import Home from './pages/Home';
 
 function App() {
   return (
-    <div><h1>Hello ssss</h1></div>
-    //  <Router>
-    //    <Route exact path="/" component={Home} />
-    //  </Router>
-    //fix this too ^^
+      <>
+      <div>
+      <h1>Frosty Penguins</h1>
+      <ButtonComponent 
+      content="Major Determinator" 
+      cssClass='e-outline'
+      onClick={MajorDeterminator}
+      ></ButtonComponent>
+      <ButtonComponent content="Schedule Builder" cssClass='e-outline'></ButtonComponent>
+      <ButtonComponent content="Major Navigator" cssClass='e-outline'></ButtonComponent>
+      <ButtonComponent content="Classes Navigator" cssClass='e-outline'></ButtonComponent>
+    </div>
+    </>
   );
 }
 
 export default App;
+
