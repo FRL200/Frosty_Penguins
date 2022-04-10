f6caa17392d8aee9ea65a1b187274e86a54788a7
JoeParimucha <JoeParimucha@gmail.com>
2022-04-10 10:36:08 -0400

    New Buttons and Set up Background. Will be improving everything over the coming days.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 
 //Imports for React, router, and buttons--------------------
 import React from 'react';
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
 import { ButtonComponent } from '@syncfusion/ej2-react-buttons';
+import {enableRipple} from '@syncfusion/ej2-base'
 //----------------------------------------------------------
 
 
@@ -14,19 +15,27 @@ import NoPage from "./pages/NoPage";
 import MajorDeterminator from "./pages/MajorDeterminator";
 import MajorNavigator from "./pages/MajorNavigator";
 import ClassesNavigator from "./pages/ClassesNavigator";
+import image from "./images/BackgroundImage.png"
 //-------------------------------------------------------
 
 //Imports for CSS Files----------------------------------
 import './App.css';
 //-----------------------------------------------------
 
-
-
 //I also had to change App to a class for this to work, hopefully it does not cause any issues
+
+const divStyle = {
+    width: '88%',
+    height: '800px',
+    backgroundImage: `url(${image})`,
+    backgroundSize: 'cover'
+};
+
 
 class App extends React.Component {
     //return (
         //<div>
+
 
 
     //Essentially, when a button is clicked, the onClick in the render() 
@@ -49,28 +58,46 @@ class App extends React.Component {
     }
 
 
+    //<div style={{ backgroundImage: `url(${image})` }}> </div>
+    //return (<div style={{ backgroundImage:`url(${image})`}}>
 
     //This render window sets up the button components and the routing paths so the user can navigate
     render() {
-        return (<div>
-            <ButtonComponent cssClass='e-link' onClick={this.btnClickHome.bind(this)}>Home</ButtonComponent>
-            <ButtonComponent cssClass='e-link' onClick={this.btnClickMajorDeterminator.bind(this)}>Major Determinator</ButtonComponent>
-            <ButtonComponent cssClass='e-link' onClick={this.btnClickMajorNav.bind(this)}>Major Navigator</ButtonComponent>
-            <ButtonComponent cssClass='e-link' onClick={this.btnClickClassesNav.bind(this)}>Classes Navigator</ButtonComponent>
+        /*
+        const styleBackground = {
+            backgroundImage:`url(${image})`,
+            height: '100vh',
+            fontSize: '50px',
+            backgroundSize: 'cover',
+        };
 
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Layout />}>
-                        <Route index element={<Home />} />
-                        <Route path="*" element={<NoPage />} />
-                        <Route path="MajorDeterminator" element={<MajorDeterminator />} />
-                        <Route path="MajorNavigator" element={<MajorNavigator />} />
-                        <Route path="ClassesNavigator" element={<ClassesNavigator />} />
-                    </Route>
-                </Routes>
-            </BrowserRouter>
 
-        </div>);
+       return (<div style={styleBackground}>
+*/
+
+        return (
+
+        <>
+            <section className="main-container">
+                <ButtonComponent cssClass="e-custom" onClick={this.btnClickHome.bind(this)}>Home</ButtonComponent>
+                <ButtonComponent cssClass="e-custom1" onClick={this.btnClickMajorDeterminator.bind(this)}>Major Determinator</ButtonComponent>
+                <ButtonComponent cssClass="e-custom2" onClick={this.btnClickMajorNav.bind(this)}>Major Navigator</ButtonComponent>
+                <ButtonComponent cssClass="e-custom3" onClick={this.btnClickClassesNav.bind(this)}>Classes Navigator</ButtonComponent>
+
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<Layout />}>
+                            <Route index element={<Home />} />
+                            <Route path="*" element={<NoPage />} />
+                            <Route path="MajorDeterminator" element={<MajorDeterminator />} />
+                            <Route path="MajorNavigator" element={<MajorNavigator />} />
+                            <Route path="ClassesNavigator" element={<ClassesNavigator />} />
+                        </Route>
+                    </Routes>
+                        </BrowserRouter>
+            </section>
+        </>
+       );
     }
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,33 @@
-import {React} from 'react';
+import { React } from 'react';
+import './styling/Home.css'
+
+
 
 //This page sets up the home page for a user to navigate to
 const Home = () => {
     return <>
-        <h1>Home</h1>
-        <p1>This is the Home Page!</p1>
+        <div>
+            <h1>Home</h1>
+                <p>This is the Home Page! Here, you will find all of the information about our product!</p>
+                <p>You may be wondering what this website has to offer. Well, you can find all of that information below!</p>
+            <h1>Major Determinator</h1>
+                <p>Our Major Determinator allows you to answer a quick questionaire to tell you what majors suit you best!</p>
+            <h1>Major Navigator</h1>
+                <p>Our Major Navigator allows you to look through majors, their requirements, and classes, to see if you are interested in them!</p>
+            <h1>Classes Navigator</h1>
+            <p>Our Classes Navigator allows you to search through different classes and what they have to offer!</p>
+            <div id="footer">
+                <p>Hope you enjoy our website!</p>
+            </div>
+            </div>
         </>;
 };
+
+    //document.getElementById("heading1").style.fontSize = "large";
+
+
+
+
 
 export default Home;
 
