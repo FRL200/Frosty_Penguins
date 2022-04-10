0ac27dd60d64097e0852278e3a6bcc0dd2b4fc20
JoeParimucha <JoeParimucha@gmail.com>
2022-04-10 18:21:40 -0400

    Updated Homepage with background, navigation, text styles, and more.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,22 +5,112 @@ import './styling/Home.css'
 
 //This page sets up the home page for a user to navigate to
 const Home = () => {
+
     return <>
-        <div>
-            <h1>Home</h1>
-                <p>This is the Home Page! Here, you will find all of the information about our product!</p>
-                <p>You may be wondering what this website has to offer. Well, you can find all of that information below!</p>
-            <h1>Major Determinator</h1>
-                <p>Our Major Determinator allows you to answer a quick questionaire to tell you what majors suit you best!</p>
-            <h1>Major Navigator</h1>
+        <html>
+            <div style={{
+                alignItems: "center",
+                justifyContent: "center",
+                color: "White",
+                fontSize: "50px"
+
+            }}>
+                <h1 align="center">Home</h1>
+            </div>
+
+            <div style={{
+                alignItems: "center",
+                justifyContent: "center",
+                color: "White",
+                fontSize: "25px",
+                marginBottom: "50px"
+
+            }}>
+                <p align="center">This is the Home Page! Here, you will find all of the information about our product!</p>
+                <p align="center">You may be wondering what this website has to offer. Well, you can find all of that information below!</p>
+
+            </div>
+
+            <div style={{
+                alignItems: "center",
+                justifyContent: "center",
+                color: "White",
+                fontSize: "35px"
+
+            }}>
+                <h1>Major Determinator</h1>
+
+            </div>
+
+
+           
+            <div style={{
+                alignItems: "center",
+                justifyContent: "center",
+                color: "White",
+                fontSize: "20px",
+                marginBottom: "30px"
+            }}>
+
+
+            <p>Our Major Determinator allows you to answer a quick questionaire to tell you what majors suit you best!</p>
+
+            </div>
+
+
+
+
+            <div style={{
+                alignItems: "center",
+                justifyContent: "center",
+                color: "White",
+                fontSize: "35px"
+
+            }}>
+                <h1>Major Navigator</h1>
+
+            </div>
+
+
+            <div style={{
+                alignItems: "center",
+                justifyContent: "center",
+                color: "White",
+                fontSize: "20px",
+                marginBottom: "30px"
+            }}>
+
                 <p>Our Major Navigator allows you to look through majors, their requirements, and classes, to see if you are interested in them!</p>
-            <h1>Classes Navigator</h1>
-            <p>Our Classes Navigator allows you to search through different classes and what they have to offer!</p>
-            <div id="footer">
-                <p>Hope you enjoy our website!</p>
+
+
             </div>
+
+
+            <div style={{
+                alignItems: "center",
+                justifyContent: "center",
+                color: "White",
+                fontSize: "35px"
+
+            }}>
+                    <h1>Classes Navigator</h1>
+
             </div>
+
+            <div style={{
+                alignItems: "center",
+                justifyContent: "center",
+                color: "White",
+                fontSize: "20px"
+            }}>
+
+                <p>Our Classes Navigator allows you to search through different classes and what they have to offer!</p>
+
+                </div>
+
+        </html>
         </>;
+
 };
 
     //document.getElementById("heading1").style.fontSize = "large";
