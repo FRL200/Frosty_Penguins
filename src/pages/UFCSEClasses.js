1ecec839cea6c50663bd5ab44ebf6e91a7cd3d45
Eitan Kogutek <eitan.kogutek@ufl.edu>
2022-04-14 19:49:34 -0400

    Worked on major explorer

---
+++ src/pages/UFCSEClasses.js
@@ -0,0 +1,14 @@
+
+//This page sets up a Classes Navigator page for any user to use
+
+
+const UFCSEClasses = () => {
+    return <>
+        <h1>UF CSE Classes</h1>
+        <p1>This is the UF CSE Classes Page!</p1>
+
+        
+    </>;
+};
+
+export default UFCSEClasses;
